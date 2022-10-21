3a1dd9521f04b5cb440bf16995aa2ab41379508d
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-10-21 18:24:30 -0300

    Iniciando ajustes CSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Card } from 'primereact/card'
+import "./Accordion.css" 
 
 const Accordion = ( { itens }) => {
     const expressaoJSX = itens.map((item, indice) => {
         return (
-            <Card key={indice} className="border-1 border-400">
+            <Card id="accordion" key={indice} className="border-1 border-400">
                 <div>
                     <i className="pi pi-angle-down"></i>
                     <h5 className="inline ml-3">{item.titulo}</h5>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 import "primeflex/primeflex.css";
+import "./components/Accordion.css"
 
 ReactDOM.render(
   <App />,
